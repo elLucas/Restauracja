0f02f8a2553210f060c709bbd57c8d777bf6867a
kku <karol777@gmail.com>
2011-12-14 20:51:32 +0100

    warstwa biznesowa

---
+++ Restauracja/Restauracja.BusinessLayer/DataModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Restauracja.BusinessLayer
+{
+    public class DataModel
+    {
+        //TODO stworzyc baze i dodac modele
+    }
+}
